ef162c4470c8704e8b2fcec6c390bebc18eea359
ncordero282 <84489532+ncordero282@users.noreply.github.com>
2026-01-31 11:08:27 -0500

    Update AutopilotScript.ps1

--- AutopilotScript.ps1
+++ AutopilotScript.ps1
@@ -1,35 +1,37 @@
 # send email to EnterpriseMobileDeviceManagement@oti.nyc.gov for support
- 
+
 $global:clientId = "7ee59b78-92d6-45e0-a2d9-a530fecbd6d3"
-$global:authUrl = "https://login.microsoftonline.com/nyco365.onmicrosoft.com";
-$global:resource = "https://graph.microsoft.com/";
-$global:webhookurl = "https://80251b4f-5295-4911-a0d0-3e0e3692a407.webhook.eus2.azure-automation.net/webhooks?token=Z1srgtPU1t8r55KAysdUVYEESJ88TwPwuld4MqYtyjs%3d%22
- 
+$global:authUrl = "https://login.microsoftonline.com/nyco365.onmicrosoft.com"
+$global:resource = "https://graph.microsoft.com/"
+$global:webhookurl = "https://80251b4f-5295-4911-a0d0-3e0e3692a407.webhook.eus2.azure-automation.net/webhooks?token=Z1srgtPU1t8r55KAysdUVYEESJ88TwPwuld4MqYtyjs%3d%22"
+
 $global:Devicecode = $null
 $global:Token = $null
- 
+
 function Request-DeviceCode {
- 
     if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $false) {
         Write-Host "Please run PowerShell in elevated mode"
         return
     }
- 
+
     $postParams = @{ resource = "$global:resource"; client_id = "$global:clientId" }
-    $DevicecodeResponse = Invoke-RestMethod -Method POST -Uri "$global:authUrl/oauth2/devicecode" -Body $postParams
-    $global:Devicecode = $DevicecodeResponse
-    Write-Host "From your managed device, " $DevicecodeResponse.message
- 
-    # Open the device login URL
-    Start-Process "https://microsoft.com/devicelogin"
- 
-    # Prompt the user to enter the code at the opened URL
-    Write-Host "Please enter the code at the opened URL: $($DevicecodeResponse.user_code)"
-    Read-Host "Press Enter after you have entered the code to continue..."
+    try {
+        $DevicecodeResponse = Invoke-RestMethod -Method POST -Uri "$global:authUrl/oauth2/devicecode" -Body $postParams
+        $global:Devicecode = $DevicecodeResponse
+        Write-Host "From your managed device, " $DevicecodeResponse.message
+
+        # Open the device login URL
+        Start-Process "https://microsoft.com/devicelogin"
+
+        # Prompt the user to enter the code at the opened URL
+        Write-Host "Please enter the code at the opened URL: $($DevicecodeResponse.user_code)"
+        Read-Host "Press Enter after you have entered the code to continue..."
+    } catch {
+        Write-Error "Failed to request device code. Check internet connection."
+    }
 }
- 
+
 function Get-Token {
- 
     $tokenParams = @{ grant_type = "device_code"; resource = "$global:resource"; client_id = "$global:clientId"; code = "$($global:Devicecode.device_code)" }
     try {
         $tokenResponse = Invoke-RestMethod -Method POST -Uri "$global:authUrl/oauth2/token" -Body $tokenParams
@@ -39,25 +41,24 @@ function Get-Token {
         return
     }
 }
- 
+
 function SendTo-Autopilot {
- 
     Get-Token
- 
+
     if ($Global:Token -eq $null) {
         Write-Host "You didn't authenticate to Azure AD, please start over."
         return
     }
- 
+
     if ((New-Object Security.Principal.WindowsPrincipal([Security.Principal.WindowsIdentity]::GetCurrent())).IsInRole([Security.Principal.WindowsBuiltInRole]::Administrator) -eq $false) {
         Write-Host "Please run PowerShell in elevated mode"
         return
     }
- 
+
     $DeviceHashData = (Get-WmiObject -Namespace "root/cimv2/mdm/dmmap" -Class "MDM_DevDetail_Ext01" -Filter "InstanceID='Ext' AND ParentID='./DevDetail'" -Verbose:$false).DeviceHardwareData
     $SerialNumber = (Get-WmiObject -Class "Win32_BIOS" -Verbose:$false).SerialNumber
     Write-Host "Device Serial Number: " $SerialNumber
- 
+
     $id_token = $global:Token.id_token
     $token_type = $global:Token.token_type
     $access_token = $global:Token.access_token
@@ -68,7 +69,7 @@ function SendTo-Autopilot {
         "id_token" = "$id_token"; 
         "access_token" = "$access_token"; 
     }
- 
+
     $params = @{
         ContentType = 'application/json'
         Headers = @{ 'Date' = "$(Get-Date)"; }
@@ -76,7 +77,7 @@ function SendTo-Autopilot {
         Method = 'Post'
         URI = $global:webhookurl
     }
- 
+
     try {
         Invoke-RestMethod @params
         Start-Sleep -Seconds 3
@@ -85,7 +86,7 @@ function SendTo-Autopilot {
         Write-Host "Failed to send data to Autopilot. Please check your network connection or try again later."
     }
 }
- 
-# Call the functions to execute the script
+
+# --- EXECUTION ---
 Request-DeviceCode
 SendTo-Autopilot
